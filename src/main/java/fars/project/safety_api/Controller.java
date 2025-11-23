d3e7b3f2332d0b682112b423ef919eb4eef58b29
Fars <fars.bein@ryerson.ca>
2025-11-22 20:13:56 -0500

    added score end point

--- src/main/java/fars/project/safety_api/Controller.java
+++ src/main/java/fars/project/safety_api/Controller.java
@@ -12,23 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-    @GetMapping("api/test/{resourceId}")
-    public String getString(@PathVariable String resourceId){
-        return "Here is a string for you :0: " + resourceId;
-    }
-
-    @PostMapping("api/set-profile")
-    public String setProfile(@RequestBody Profile profile) {
-        System.out.println("Here is a string for you :0: " + profile);
-        return "Profile received: Name - " + profile.getName() + ", Age - " + profile.getAge();
-    }
-
-    @PostMapping("api/simple/json")
-    public String setProfile(@RequestBody String body) {
-        System.out.println("Raw JSON body: " + body);
-        return "Received body: " + body;
-    }
-
     @GetMapping("api/health")
     public Map<String, Boolean> health() {
         Map<String, Boolean> response = new HashMap<>();
@@ -46,39 +29,66 @@ public class Controller {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
         return jsonData;
     }
 
 
-    @PostMapping("api/score/{LatAndLong}")
-    public Map<String, Double> scoreByLatAndLong(@PathVariable String LatAndLong) {
-        Map<String, Double> response = new HashMap<>();
+    @GetMapping("api/score")
+    public Map<String, Object> scoreBasedOnLatAndLong(@RequestParam double lat, @RequestParam double lng) {
+        final int ReportsRange = 500;
+
+        Map<String, Object> response = new HashMap<>();
+        Map<String, Double> location = new HashMap<>();
+        location.put("lat", lat);
+        location.put("lng", lng);
+
+        response.put("radius_meters", ReportsRange);
+        response.put("time_range_years", 2);
+        response.put("location", location);
+
+        JsonNode assaultData = null;
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            assaultData = objectMapper.readTree(Paths.get("data/assaults.json").toFile());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<Object> ReportsNearCurrentCoordinates = new ArrayList<>();
+        for (JsonNode report : assaultData) {
+            double reportLat = report.get("LAT_WGS84").asDouble();
+            double reportLong = report.get("LONG_WGS84").asDouble();
+
+            if (distanceInMeters(reportLat, reportLong, lat, lng) > ReportsRange) continue;
+
+            ReportsNearCurrentCoordinates.add(report);
+        }
+        response.put("report_count", ReportsNearCurrentCoordinates.size());
+        response.put("safety_level", safetyLevel(ReportsNearCurrentCoordinates, ReportsRange));
+        response.put("reports", ReportsNearCurrentCoordinates);
         return response;
     }
-}
 
-class Profile {
-    private String name;
-    private String age;
-
-    public String getName(){
-        return name;
+    private double distanceInMeters(double lat1, double lon1, double lat2, double lon2) {
+        // (FALSE) using the distance formula, to find the distance between to points on x and y coordinate plane
+        // using Haversine formula, because earth is sphere (not flat) thous degrees aren't linear and most be adjusted in order to be converted it into meters https://en.wikipedia.org/wiki/Haversine_formula
+        final double R = 6371000;
+        double x = Math.toRadians(lon2 - lon1) * Math.cos(Math.toRadians((lat1 + lat2) / 2));
+        double y = Math.toRadians(lat2 - lat1);
+        return Math.sqrt(x * x + y * y) * R;
     }
 
-    public void setName(String name) {
-        this.name= name;
-    }
-
-    public void setAge(String age) {
-        this.age= age;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    @Override
-    public String toString() {
-        return "Profile{name='" + name + "', age='" + age + "'}";
+    private String safetyLevel(List<Object> reports, int rangeMeters) {
+        //TODO: That take into account the recency of the reports
+        //TODO: That take into account the distance of the reports for now we have a fixed range of 500 meters
+        int count = reports.size();
+        if (count == 0) return "Very Safe";
+        else if (count <= 3) return "Safe";
+        else if (count <= 7) return "Moderate";
+        else if (count <= 15) return "Unsafe";
+        else return "Very Unsafe";
     }
 }
